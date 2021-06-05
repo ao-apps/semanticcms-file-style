b55e72bb9a9e596a2791688cd022274ce09fb80e
AO Industries, Inc <support@aoindustries.com>
2021-06-05 01:41:01 -0500

    Open-source projects moved to "com.aoapps" packages

--- src/main/java/com/semanticcms/file/style/FileStyle.java
+++ src/main/java/com/semanticcms/file/style/FileStyle.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-file-style - Default style for files nested within SemanticCMS pages and elements.
- * Copyright (C) 2016, 2017, 2020  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2020, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,10 +22,10 @@
  */
 package com.semanticcms.file.style;
 
-import com.aoindustries.net.Path;
-import com.aoindustries.web.resources.registry.Group;
-import com.aoindustries.web.resources.registry.Style;
-import com.aoindustries.web.resources.servlet.RegistryEE;
+import com.aoapps.net.Path;
+import com.aoapps.web.resources.registry.Group;
+import com.aoapps.web.resources.registry.Style;
+import com.aoapps.web.resources.servlet.RegistryEE;
 import com.semanticcms.core.servlet.SemanticCMS;
 import com.semanticcms.file.model.File;
 import javax.servlet.ServletContext;
